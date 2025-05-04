3ed537f055e4a7af28982011d942dad5586a9663
LavinduNadungamuwa <lavindunadungamuwa@gmail.com>
2025-05-04 22:40:55 +0530

    Adding time feature

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const date = document.getElementById("date");
 const day = document.getElementById("day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
+const time = document.getElementById("time");
 
 const today = new Date();
 
@@ -12,3 +13,14 @@ date.innerHTML = String(today.getDate()).padStart(2, '0');
 day.innerHTML = weekDays[today.getDay()];
 month.innerHTML = allMonths[today.getMonth()];
 year.innerHTML = today.getFullYear();
+
+function updateTime() {
+    const now = new Date();
+    let hrs = String(now.getHours()).padStart(2, '0');
+    let mins = String(now.getMinutes()).padStart(2, '0');
+    let secs = String(now.getSeconds()).padStart(2, '0');
+    time.innerHTML = `${hrs}:${mins}:${secs}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
